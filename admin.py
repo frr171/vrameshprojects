29dcedb00ee39d1a32c3e84b2998ad6f75eed7f1
Varun Ramesh <vramesh@tgen.org>
2013-07-22 14:07:07 -0700

    Tags are now added to a project using checkboxes

--- admin.py
+++ admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from projects.models import *
 
+from django.forms import CheckboxSelectMultiple
+
 class ProjectAdmin(admin.ModelAdmin):
 	list_display = ('title', 'description', 'year')
 	list_filter = ['year']
 	
+	#Set the tags field to be checkboxes
+	formfield_overrides = {
+        models.ManyToManyField: {'widget': CheckboxSelectMultiple},
+    }
+
+#Register all of the models to be editable from the admin console
 admin.site.register(Category)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(List)
